aa3ea7f952372b24c06c8263869d94c7245eff5f
Wahaj-Khan <wahajkhan108@gmail.com>
2023-09-26 22:24:07 +0500

    Final

---
+++ Restaurant-Order-App-main/screens/Home.js
@@ -0,0 +1,13 @@
+import MenuItems from "../shared/MenuItems";
+import HeroSection from '../shared/HeroSection';
+
+const Home = () => {
+    return (
+        <>
+            <HeroSection />
+            <MenuItems />
+        </>
+    );
+}
+
+export default Home;
